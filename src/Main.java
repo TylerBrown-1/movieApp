a30018baa5133bf1eb08161a2d1ec4c52f9aa547
TylerBrown-1 <browntylerr1@gmail.com>
2024-09-25 12:43:44 -0500

    movie app

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+
+       System.out.println("Hello user");
+        String movieName = " Friday ";
+        String directorsName = " ice cube ";
+        int releaseYear = 1999 ;
+        double imdbRating = 10.0;
+        int reviews = 199;
+        boolean isSeries = true;
+        char directorsIntial = 'C';
+
+
+        System.out.println( " My favorite movie is: " + movieName);
+        System.out.println( " The directors name is " + directorsName);
+        System.out.println( " The movie came out in year " + movieName);
+        System.out.println("The movie rating is " + imdbRating);
+        System.out.println(" The number of reviews is  " + reviews);
+        System.out.println("Is the movie apart of a Series? " + isSeries );
+/*
+        String username = "user";
+        String password = "word";
+        String genrePreference = "Superhero Movies";
+        String[] actionMovies = {
+                "Spooderman",
+                "Death pond",
+                "Wolf on the Revine",
+                "Hulk"};
+String[] favGenre = {"Action", "Comedy", "Thriller"};
+
+ng[] favoriteGenres = {"Action", "Comedy", "Animated"};
+System.out.println("First movie" + favGenre[0]); //first item in my array
+System.out.println("First movie" + favGenre[2]); //last item in my array
+System.out.println("First movie" + favGenre[1]); //last item in my array
+        String releaseYear = "2004";
+        byte movieRatingStars = 4;
+        int numberOfReviews = 246_487;
+        long watch = 6543543435443444343L;
+
+        System.out.println("Hi " + username);
+        System.out.println("What kind of " + genrePreference + "would you like to see today?" );
+        System.out.println("Title 2 =" + actionMovies[1]);
+
+ */
+ */
+        System.out.println("The movie " + actionMovies[0] + " was released in " + releaseYear + " with a " + movieRatingStars + " star rating, " + numberOfReviews + " reviews and has been watched " + watch + " number of times in total by users on this site!");
+/*
+}
+}
